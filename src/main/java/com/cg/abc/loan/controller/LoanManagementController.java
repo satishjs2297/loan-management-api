97256b32212d0e37f5a6b58d1acdf6e7fca9bb1b
Satish Y <satishjs2297@gmail.com>
2019-08-31 17:23:40 +0530

    Fix api response issues.

--- src/main/java/com/cg/abc/loan/controller/LoanManagementController.java
+++ src/main/java/com/cg/abc/loan/controller/LoanManagementController.java
@@ -3,6 +3,8 @@ package com.cg.abc.loan.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,37 +27,38 @@ public class LoanManagementController {
 	private LoanService loanService;
 
 	@PostMapping("/applyLoan")
-	public String processLoan(@RequestBody Loan loan) {
+	public ResponseEntity<String> processLoan(@RequestBody Loan loan) {
 		loanService.applyLoan(loan);
-		System.out.println("Loan has been successfully processed....");
-		return "Loan With Account Number : "+ loan.getAccountNumber() + " has been successfully applied";
+		System.out.println("Loan With Account Number : " + loan.getAccountNumber() + " has been successfully applied");
+		return new ResponseEntity<String>(loan.getAccountNumber(), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/getLoanById/{accountNumber}")
 	public Loan getLoanById(@PathVariable String accountNumber) {
-		return loanService.getLoanById(accountNumber);	
+		return loanService.getLoanById(accountNumber);
 	}
-	
+
 	@GetMapping("/getAllLoans")
 	public List<Loan> getAllLoans() {
-		return loanService.getAllLoans();	
-	}	
-	
-	@PutMapping("/payEmi/{accountNumber}")
-	public String payEmi(@PathVariable String accountNumber) {
-		return loanService.payEmi(accountNumber);
+		return loanService.getAllLoans();
 	}
-	
+
+	@PutMapping("/payEmi/{accountNumber}")
+	public ResponseEntity<String> payEmi(@PathVariable String accountNumber) {
+		return new ResponseEntity<String>(loanService.payEmi(accountNumber), HttpStatus.OK);
+	}
+
 	@GetMapping("/calculate/{loanAmount}/{interestRate}/{time}")
-	public double calculateEmi(@PathVariable double loanAmount, @PathVariable double interestRate, @PathVariable int time) {
+	public double calculateEmi(@PathVariable double loanAmount, @PathVariable double interestRate,
+			@PathVariable int time) {
 		return loanService.calculateEmi(loanAmount, interestRate, time);
 	}
-	
+
 	@PutMapping("/forceclose/{accountNumber}")
-	public String forceClose(@PathVariable String accountNumber) {
-		return loanService.foreClose(accountNumber);
+	public ResponseEntity<String> forceClose(@PathVariable String accountNumber) {
+		return new ResponseEntity<String>(loanService.foreClose(accountNumber), HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/getAllLoanTransactions")
 	public List<LoanTransaction> getAllTransactions() {
 		return loanService.getAllLoanTransactions();
